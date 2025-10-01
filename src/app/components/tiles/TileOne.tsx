1918e513ecb2cacf8f51f7052f9aad50f3270a0d
murayama <murayama333@gmail.com>
2025-10-02 06:25:22 +0900

    update

--- src/app/components/tiles/TileOne.tsx
+++ src/app/components/tiles/TileOne.tsx
@@ -3,10 +3,9 @@ type Props = { html: string };
 export default function TileOne({ html }: Props) {
   return (
     <div
-      className="common one bg-white fixed left-0 top-0 w-[60vw] h-screen p-8 z-[1] flex flex-col gap-1 overflow-y-scroll [&_ul]:text-[1.5rem] [&_p]:text-[1.5rem] [&_pre_code]:rounded-[10px] [&_pre_code]:text-[1.20rem] [&_pre_code]:bg-neutral-800"
+      className="common bg-white fixed left-0 top-0 w-[60vw] h-screen :bg-neutral-800"
       contentEditable={true}
       dangerouslySetInnerHTML={{ __html: html }}
     ></div>
   );
 }
-
--- src/app/components/tiles/TileTwo.tsx
+++ src/app/components/tiles/TileTwo.tsx
@@ -3,8 +3,10 @@ type Props = { html: string; open: boolean; onToggleSize: () => void };
 export default function TileTwo({ html, open, onToggleSize }: Props) {
   return (
     <div
-      className={`common two fixed right-0 top-0 z-[2] overflow-y-scroll flex flex-col items-start justify-center gap-4 bg-[#eee] transition-opacity duration-500 ease-in-out [&_h3]:text-[1.2rem] [&_h3]:text-left [&_img]:max-w-full [&_ul]:text-[1.5rem] [&_p]:text-[1.5rem] [&_pre_code]:rounded-[10px] [&_pre_code]:text-[1.20rem] [&_pre_code]:bg-neutral-800 ${
-        open ? "opacity-100 pointer-events-auto w-[40vw] h-screen p-8" : "opacity-0 pointer-events-none w-[40vw] h-screen p-8"
+      className={`common fixed right-0 top-0 z-[2] bg-[#eee]  ${
+        open
+          ? "opacity-100 pointer-events-auto w-[40vw] h-screen p-8"
+          : "opacity-0 pointer-events-none w-[40vw] h-screen p-8"
       }`}
       contentEditable={true}
       onDoubleClick={onToggleSize}
@@ -12,4 +14,3 @@ export default function TileTwo({ html, open, onToggleSize }: Props) {
     ></div>
   );
 }
-
