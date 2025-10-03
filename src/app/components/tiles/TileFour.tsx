1b416534455c92e88e5431937dd4ddaa200d1592
murayama <murayama333@gmail.com>
2025-10-03 14:18:50 +0900

    update

--- src/app/components/tiles/TileFour.tsx
+++ src/app/components/tiles/TileFour.tsx
@@ -5,7 +5,7 @@ export default function TileFour({ html, open, fade = true }: Props) {
     <div
       className={`common ${
         fade ? "fade" : ""
-      } bg-gradient-to-b from-slate-900 to-slate-800 justify-center text-white fixed bottom-0 left-0 z-[200] w-[100vw] ${
+      } bg-teal-950 justify-center text-white fixed bottom-0 left-0 z-[200] w-[100vw] ${
         open
           ? "opacity-95 pointer-events-auto p-8"
           : "opacity-0 pointer-events-none p-8"
